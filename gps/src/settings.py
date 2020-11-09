57a2d803bdc0aa50cdc6c24687b77f51776e5028
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2020-11-09 17:18:47 +0100

    Gps config

--- gps/src/settings.py
+++ gps/src/settings.py
@@ -7,25 +7,25 @@ class Settings(CommonSettings):
         return self._values['latitude_timing_start'] \
             if self._values.__contains__('latitude_timing_start') \
                and isinstance(self._values['latitude_timing_start'], float) \
-            else '45.032888'
+            else 45.032888
 
     @property
     def longitude_timing_start(self) -> float:
         return self._values['longitude_timing_start'] \
             if self._values.__contains__('longitude_timing_start') \
                and isinstance(self._values['longitude_timing_start'], float) \
-            else '7.792347'
+            else 7.792347
 
     @property
     def latitude_timing_end(self) -> float:
         return self._values['latitude_timing_end'] \
             if self._values.__contains__('latitude_timing_end') \
                and isinstance(self._values['latitude_timing_end'], float) \
-            else '45.032888'
+            else 45.032888
 
     @property
     def longitude_timing_end(self) -> float:
         return self._values['longitude_timing_end'] \
             if self._values.__contains__('longitude_timing_end') \
                and isinstance(self._values['longitude_timing_end'], float) \
-            else '7.792347'
+            else 7.792347
