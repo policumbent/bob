f206a0b4d1b35b1023d85477813b1a1c5a2269a5
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2021-09-21 20:48:10 +0200

    Modulo indoor_weather

--- weather/src/weather.py
+++ weather/src/weather.py
@@ -7,15 +7,9 @@ import threading
 
 from threading import Thread
 import time
-import board
-import digitalio
-from busio import SPI
-from board import SCK, MOSI, MISO, D5, D9, D10, D11
-from digitalio import DigitalInOut as dgio
-from adafruit_bmp280 import Adafruit_BMP280_SPI
 
-from src.sensors.sensor import Sensor
-from src.utility.settings import Settings
+from .common_files.sensor import Sensor
+from .settings import Settings
 try:
     from smbus2 import SMBus
 except ImportError:
@@ -30,15 +24,16 @@ class Weather(Sensor):
     def signal(self, value: str):
         pass
 
-    def __init__(self, settings: Settings, loop_delay: int = 1):
+    def __init__(self, settings: Settings,  send_alert,
+                 send_message, loop_delay: int = 1):
         self._bus = SMBus(1)
         self._settings = settings
+        self._send_alert = send_alert
+        self._send_message = send_message
         self._settings_lock = threading.Lock()
         self._value_lock = threading.Lock()
         self._sensor = BME280(i2c_dev=self._bus)
         self._data = dict()
-        self._data["wind_speed"] = 100
-        print('Ciaooooooooo')
         self._worker = Thread(target=self._loop, daemon=False)
         self._worker.start()
 
