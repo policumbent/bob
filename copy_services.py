343b6c58095339c4c65fc64d707a12081f52c1fc
davide.aimar <davide.aimar99@gmail.com>
2021-10-20 17:06:00 +0200

    fix: disabled auto enable

--- copy_services.py
+++ copy_services.py
@@ -9,7 +9,7 @@ for file_path in files:
     service_name = os.path.basename(file_path)
     new_path = os.path.join(dest, service_name)
     shutil.copy(file_path, new_path)
-    os.system(f'systemctl enable {service_name}')
+    # os.system(f'systemctl enable {service_name}')
 
 os.system('systemctl daemon-reload')
 
