220cf72deb94f0cbff7eeee41ee7cb5a913db8e2
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2022-08-05 15:39:39 +0200

    add: mqtt api

--- modules/ant/src/main.py
+++ modules/ant/src/main.py
@@ -1,16 +1,17 @@
 import asyncio
 import threading
 import logging
+import os
 
-from core import log
+from core import log, Mqtt, Database
 
 
 from .ant.base.driver import DriverNotFound
 from .device import AntDevice, DeviceTypeID, Node
 from .heartrate import HeartRate
-from .powermeter import Powermeter
 from .hall import Hall
 
+# from .powermeter import Powermeter
 
 # disable all logging for the ant library
 logging.disable(logging.WARNING)
@@ -28,29 +29,43 @@ async def read_data(sensor):
 
 async def mqtt():
     while True:
-        if data != dict():
-            print(data)
-
-        await asyncio.sleep(0.2)
+        try:
+            async with Mqtt() as client:
+                while True:
+                    for key, value in data.items():
+                        await client.sensor_publish(f"ant/{key}", value)
+                    await asyncio.sleep(0.2)
+        except Exception as e:
+            log.err(e)
+        finally:
+            await asyncio.sleep(1)
 
 
 async def main():
     while True:
         try:
+            # retrive configurations from db
+            home_path = os.getenv("HOME")
+            db_path = os.getenv("DATABASE_PATH") or f"{home_path}/bob/database.db"
+
+            config = Database(path=db_path).config("ant")
+
+            bike = config.get("name")
+            hall_id = config.get(f"{bike}_hall_id")
+            hall_type = config.get(f"{bike}_hall_type")
+            hr_id = config.get(f"{bike}_hr_id")
+
             with Node(0x00, AntDevice.NETWORK_KEY) as node:
-                # TODO: ricavare gli id dal database di configurazione
+                hall = Hall(
+                    node, sensor_id=hall_id, device_type=DeviceTypeID(hall_type)
+                )
+                hr = HeartRate(
+                    node, sensor_id=hr_id, device_type=DeviceTypeID.heartrate
+                )
 
-                # hall phoenix
-                # hall = Hall(node, sensor_id=1, device_type=DeviceTypeID.speed)
-                # hall torella
-                hall = Hall(node, sensor_id=1, device_type=DeviceTypeID.speed_cadence)
-
-                # hr = HeartRate(
-                #     node, sensor_id=2, device_type=DeviceTypeID.heartrate
-                # )  # Todo: check id
                 # pm = Powermeter(
                 #     node, sensor_id=3, device_type=DeviceTypeID.heartrate
-                # )  # Todo: check id
+                # )
 
                 # start ant loop and data read
                 threading.Thread(target=node.start, name="ant.easy").start()
@@ -58,7 +73,7 @@ async def main():
                 # release async tasks
                 await asyncio.gather(
                     read_data(hall),
-                    # read_data(hr),
+                    read_data(hr),
                     # read_data(pm),
                     mqtt(),
                 )
