7c3fefae8cd9cde08292beda27fd851ce82adde8
Gabriele Belluardo <gabriele.belluardo@outlook.it>
2022-08-06 00:48:21 +0200

    add(ant): db write

--- modules/ant/src/main.py
+++ modules/ant/src/main.py
@@ -3,7 +3,7 @@ import threading
 import logging
 import os
 
-from core import log, Mqtt, Database
+from core import log, Mqtt, Database, time
 
 
 from .ant.base.driver import DriverNotFound
@@ -41,6 +41,33 @@ async def mqtt():
             await asyncio.sleep(1)
 
 
+async def write_db(db):
+    curr_row = None
+    row = {
+        "timestamp": str(0),
+        "speed": 0,
+        "distance": 0,
+        "cadence": 0,
+        "power": 0,
+        "heartrate": 0,
+    }
+
+    while True:
+        try:
+            row.update(data)
+
+            if row != curr_row:
+                row.update({"timestamp": time.human_timestamp()})
+                
+                db.insert("ant", list(row.values()))
+
+                curr_row = dict(row)
+        except:
+            pass
+        finally:
+            await asyncio.sleep(0.2)
+
+
 async def main():
     while True:
         try:
@@ -48,7 +75,8 @@ async def main():
             home_path = os.getenv("HOME")
             db_path = os.getenv("DATABASE_PATH") or f"{home_path}/bob/database.db"
 
-            config = Database(path=db_path).config("ant")
+            db = Database(table="ant", path=db_path, max_pending=10)
+            config = db.config("ant")
 
             bike = config.get("name")
             hall_id = config.get(f"{bike}_hall_id")
@@ -76,6 +104,7 @@ async def main():
                     read_data(hr),
                     # read_data(pm),
                     mqtt(),
+                    write_db(db),
                 )
         except DriverNotFound:
             log.err("USB not connected")
