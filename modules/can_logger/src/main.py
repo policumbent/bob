e68fd415975964d0b990a914e94b1040c67af05e
fraimparato <fra.imparato20@gmail.com>
2023-04-03 17:07:08 +0200

    add: Logger from python-can module in can_logger

--- modules/can_logger/src/main.py
+++ modules/can_logger/src/main.py
@@ -1,17 +1,18 @@
 from datetime import datetime
 import can
 
+FILE_NAME = f"can_{datetime.today().strftime('%Y-%m-%d_%H-%M-%S')}.log"
 
 def entry_point():
-    date = datetime.today().strftime('%Y-%m-%d_%H-%M-%S')
-
-    with open(f"can_{date}.log", "w") as log_file:
+    with open(FILE_NAME, "w") as log_file:
         curr_time = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
         log_file.write(f"{curr_time}\n")
 
     bus = can.Bus(interface = 'socketcan',
         channel = 'can0',
         receive_own_messages = True)
+
+    logger = can.Logger(FILE_NAME, 'w')
 
 
 if __name__ == '__main__':
