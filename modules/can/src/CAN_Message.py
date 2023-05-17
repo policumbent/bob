7b954779328506f096a387f5e4d9e54fb34b7678
fraimparato <fra.imparato20@gmail.com>
2023-05-17 23:14:58 +0200

    feat: CAN_DataType - defined class containing CAN data types attributes (dlc, decimal digits) - created dictionary where attributes for each data type are stored

--- modules/can/src/CAN_Message.py
+++ modules/can/src/CAN_Message.py
@@ -1,5 +1,4 @@
 ID_TYPE_MASK    = 0b11000000000  # not used
-
 ID_DEV_MASK     = 0b00111100000
 ID_SPEC_MASK    = 0b00000011111
 
@@ -37,13 +36,16 @@ DT_POWER        = 5
 DT_HEARTRATE    = 6
 DT_GEAR         = 7
 
+from CAN_DataType import CAN_DATA_TYPE
+
 class CAN_Message:
     def encode_data(self, data, dlc, decimal_digits):
         """
         rounding data function
         :param data: data to be rounded
+        :param dlc: number of bytes of the resulting ByteArray
         :param decimal_digits: number of input decimal digits
-        :return rounded data, formatted as int
+        :return encoded_data: ByteArray
         """
         rounded_data = int(round(data, decimal_digits) * 10**decimal_digits)
 
@@ -55,48 +57,72 @@ class CAN_Message:
 
     
     def enc_speed(self, speed):
-        """
-        encoding speed function
-        :param speed taken from MQTT server
-        :return id, bytearray:encoded_speed
-        """
-
         id = (MSG_DATA << 9) | (DEV_RPI_DATA << 5) | (RPI_HS_SPEED)
-        encoded_speed = self.encode_data(speed, 2, 2)
+
+        encoded_speed = self.encode_data(
+            speed,
+            CAN_DATA_TYPE["speed"].dlc,
+            CAN_DATA_TYPE["speed"].decimal_digits
+        )
 
         return id, encoded_speed
     
 
     def enc_distance(self, distance):
         id = (MSG_DATA << 9) | (DEV_RPI_DATA << 5) | (RPI_HS_DISTANCE)
-        encoded_distance = self.encode_data(distance, 2, 2)
+
+        encoded_distance = self.encode_data(
+            distance,
+            CAN_DATA_TYPE["distance"].dlc,
+            CAN_DATA_TYPE["distance"].decimal_digits
+        )
 
         return id, encoded_distance
 
 
     def enc_wheel_rpm(self, rpm):
         id = (MSG_DATA << 9) | (DEV_RPI_DATA << 5) | (RPI_HS_W_RPM)
-        encoded_rpm = self.encode_data(rpm, 2, 1)
+
+        encoded_rpm = self.encode_data(
+            rpm,
+            CAN_DATA_TYPE["rpm"].dlc,
+            CAN_DATA_TYPE["rpm"].decimal_digits    
+        )
 
         return id, encoded_rpm
 
 
     def enc_pedal_rpm(self, rpm):
         id = (MSG_DATA << 9) | (DEV_RPI_DATA << 5) | (RPI_SRM_P_RPM)
-        encoded_rpm = self.encode_data(rpm, 2, 1)
+
+        encoded_rpm = self.encode_data(
+            rpm,
+            CAN_DATA_TYPE["rpm"].dlc,
+            CAN_DATA_TYPE["rpm"].decimal_digits
+        )
 
         return id, encoded_rpm
 
     
     def enc_power(self, power):
         id = (MSG_DATA << 9) | (DEV_RPI_DATA << 5) | (RPI_SRM_PWR)
-        encoded_power = self.encode_data(power, 2, 1)
+
+        encoded_power = self.encode_data(
+            power,
+            CAN_DATA_TYPE["power"].dlc,
+            CAN_DATA_TYPE["power"].decimal_digits
+        )
 
         return id, encoded_power
 
 
     def enc_heartrate(self, heartrate):
         id = (MSG_DATA << 9) | (DEV_RPI_DATA << 5) | (RPI_HEART_RATE)
-        encoded_hr = self.encode_data(heartrate, 1, 0)
+
+        encoded_hr = self.encode_data(
+            heartrate,
+            CAN_DATA_TYPE["heart_rate"].dlc,
+            CAN_DATA_TYPE["heart_rate"].decimal_digits
+        )
 
         return id, encoded_hr
