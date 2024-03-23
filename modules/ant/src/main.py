e6b0cdf1160e2d1a97aea7b4173310dbf683a5bb
fraimparato <fra.imparato20@gmail.com>
2024-03-23 19:44:55 +0100

    fix: not instead of !

--- modules/ant/src/main.py
+++ modules/ant/src/main.py
@@ -87,7 +87,7 @@ def read_data(sensors):
             if(sensor[0].is_data_ready()):
                 read = sensor[0].read_data()
 
-                if !curr_data:
+                if not curr_data:
                     curr_data_mutex = 1
                     curr_data.update(read)
                     curr_data_mutex = 0
@@ -108,14 +108,14 @@ def fifo(pipe_name: str):
     while True:
         try:
             while True:
-                if !curr_data:
+                if not curr_data:
                     curr_data_mutex = 1
 
                     for key, value in curr_data.items():
                         if key != "timestamp":
                             pipe.write(f"{key}:{value}")
                     # TODO: consider adding a sleep
-                    
+
                     curr_data_mutex = 0
                 
         except Exception as e:
